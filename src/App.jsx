a611feaf8d6c570cdad4ce967077fcef265cd365
Buddynice34 <willemapril33@gmail.com>
2026-03-13 21:58:35 +0200

    Add scaffold components and update App layout
    Replace Hello world with a composed app layout in App.jsx and add placeholder components for the main sections. New components: Navbar, Hero, Categories, About, BestSelling, Services, Menu, Contact, Reviews, Blog, Footer. Each component is a simple functional React component returning a placeholder div and exported as default, enabling incremental UI development.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Categories from './components/Categories'
+import About from './components/About'
+import BestSelling from './components/BestSelling'
+import Services from './components/Services'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
+import Reviews from './components/Reviews'
+import Blog from './components/Blog'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='h-dvh w-full font-Poppins'>
-      Hello world
-    </div>
+    <>
+      <Navbar />
+      <main className='h-dvh w-full font-Poppins'>
+        <Hero />
+        <Categories />
+        <About />
+        <BestSelling />
+        <Services />
+        <Menu />
+        <Contact />
+        <Reviews />
+        <Blog />
+      </main>
+      <Footer />
+    </>
   )
 }
 
